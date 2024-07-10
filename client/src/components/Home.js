f38cf2664248072a0783b222fa196c746aa03c43
KellyAineah <kellyainea99@gmail.com>
2024-07-10 12:03:08 +0300

    added a Card and images

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Carousel from './Carousel'
+import './Home.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
   return (
     <div>
+      <p className='paragraph'>
+        <span>Your Health, Our Priority  <FontAwesomeIcon icon={faHeartbeat} style={{ marginRight: '10px' }} /></span>
+
+        </p>
       <Carousel />
+      
     </div>
   )
 }
